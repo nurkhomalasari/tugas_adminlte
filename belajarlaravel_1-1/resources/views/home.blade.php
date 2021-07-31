61787511db8dab980361e40cc5c9a785dfb6ac43
nur khomalasari <nurkhomalasari24@gmail.com>
2021-07-31 14:44:34 +0700

    tugas_adminlte

---
+++ belajarlaravel_1-1/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('template.template')
+@section('title', 'Home')
+
+@section('content')
+<h1>Assalamu'alaikum wr.wb</h1>
+<h4>Selamat Datang Teman-Teman <br>
+Salam hangat untuk kita semua :)</h4>
+@endsection
